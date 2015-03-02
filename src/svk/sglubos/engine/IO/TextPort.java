df4ea853760ff7dcefef1d2c3b533bf21a52485b
sglubos <5lubo555@azet.sk>
2015-03-02 21:13:19 +0100

    added few saving methods to TextPort.java

--- src/svk/sglubos/engine/IO/TextPort.java
+++ src/svk/sglubos/engine/IO/TextPort.java
@@ -1,15 +1,17 @@
 package svk.sglubos.engine.io;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.Reader;
 import java.util.ArrayList;
 import java.util.List;
 
 import svk.sglubos.engine.utils.Strings;
-//TODO document, exceptions, saving
+//TODO document, exceptions, saving, refactor (remove methods / reduce code duplication)
 public class TextPort {
 	public static String loadTextAsString(String path) {
 		StringBuilder ret = new StringBuilder();
@@ -194,5 +196,142 @@ public class TextPort {
 		}
 		
 		return  (String[]) ret.toArray();
+	}
+	
+	public static void saveStringToFile(String string, File file) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			
+			writer.write(string);
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveStringToFileSeparateLines(String string, File file) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			String[] strings = string.split(Strings.LINE_SEPARATOR);
+			
+			for(String s : strings) {
+				writer.write(s);
+				writer.newLine();
+			}
+			
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveStringToFileSeparateLines(String string,String separator, File file) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			String[] strings = string.split(separator);
+			
+			for(String s : strings) {
+				writer.write(s);
+				writer.newLine();
+			}
+			
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveStringToFile(String string, String path) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+			
+			writer.write(string);
+			writer.flush();
+			writer.close();
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveStringToFileSeparateLines(String string, String path) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+			String[] strings = string.split(Strings.LINE_SEPARATOR);
+			
+			for(String s : strings) {
+				writer.write(s);
+				writer.newLine();
+			}
+			
+			writer.flush();
+			writer.close();
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveStringToFileSeparateLines(String string, String separator, String path) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+			String[] strings = string.split(separator);
+			
+			for(String s : strings) {
+				writer.write(s);
+				writer.newLine();
+			}
+			
+			writer.flush();
+			writer.close();
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void saveStringArrayToFile(String[] strings, File file, boolean separateStringsByLine) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+			
+			for(String s : strings) {
+				writer.write(s);
+				if(separateStringsByLine)
+					writer.newLine();
+			}
+			
+			writer.flush();
+			writer.close();
+			
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void saveStringArrayToFile(String[] strings, String path, boolean separateStringsByLine) {
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(path)));
+		
+			for(String s : strings) {
+				writer.write(s);
+				if(separateStringsByLine)
+					writer.newLine();
+			}
+		
+			writer.flush();
+			writer.close();
+		
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
