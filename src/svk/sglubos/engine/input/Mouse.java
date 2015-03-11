e6a301106f31cef60219ec9438674a92e3cf433a
sglubos <5lubo555@azet.sk>
2015-03-11 21:32:06 +0100

    Finished the documenting of Mouse.java class and renamed the Component to which mouse is bound

--- src/svk/sglubos/engine/input/Mouse.java
+++ src/svk/sglubos/engine/input/Mouse.java
@@ -61,7 +61,7 @@ public class Mouse extends MouseAdapter{
 	 *  @see #bind(Component)
 	 *  @see #unbind()
 	 */
-	private static Component bouding;
+	private static Component component;
 	
 	/**
 	 * This {@link java.util.Map Map} keeps track of state of mouse buttons.<br>
@@ -75,7 +75,7 @@ public class Mouse extends MouseAdapter{
 	
 	/**
 	 * Determines if <code>Mouse</code> is bound to a {@link java.awt.Component Component}.<br>
-	 * The reference of {@link java.awt.Component Component} to which is <code>Mouse</code> bound is stored in {@link #bouding} object.<br>
+	 * The reference of {@link java.awt.Component Component} to which is <code>Mouse</code> bound is stored in {@link #component} object.<br>
 	 * To bind <code>Mouse</code> to a {@link java.awt.Component Component} use method {@link #bind(Component)}.
 	 * If the <code>Mouse</code> is already bound, the message is printed by the {@link svk.sglubos.engine.utils.debug.MessageHandler MessageHandler} and nothing changes.<br>
 	 * To unbind the <code>Mouse</code> from a {@link java.awt.Component Component} use method{@link #unbind()}.
@@ -83,12 +83,12 @@ public class Mouse extends MouseAdapter{
 	 * 
 	 * @see #bind(Component)
 	 * @see #unbind()
-	 * @see #bouding
+	 * @see #component
 	 */
 	private static boolean bound;
 	
 	/**
-	 * Contains the x coordinate of cursor relative to {@link #bouding component} to which is <code>Mouse</code> bound.<br>
+	 * Contains the x coordinate of cursor relative to {@link #component component} to which is <code>Mouse</code> bound.<br>
 	 * To obtain value of this variable use {@link #getX()} method.
 	 * 
 	 * @see #getX()
@@ -96,7 +96,7 @@ public class Mouse extends MouseAdapter{
 	private static int x;
 	
 	/**
-	 * Contains the x coordinate of cursor relative to {@link #bouding component} to which is <code>Mouse</code> bound.<br>
+	 * Contains the x coordinate of cursor relative to {@link #component component} to which is <code>Mouse</code> bound.<br>
 	 * To obtain value of this variable use {@link #getY()} method.
 	 * 
 	 * @see #getY()
@@ -129,12 +129,12 @@ public class Mouse extends MouseAdapter{
 	private static boolean mouseWheelRotated;
 	
 	/**
-	 * Determines if cursor is inside of {@link #bouding component} to which is <code>Mouse</code> bound.<br>
-	 * If the cursor is inside of {@link #bouding that component} the value of this variable is <code>true</code> else the value is <code>false</code>.<br>
+	 * Determines if cursor is inside of {@link #component component} to which is <code>Mouse</code> bound.<br>
+	 * If the cursor is inside of {@link #component that component} the value of this variable is <code>true</code> else the value is <code>false</code>.<br>
 	 * To obtain the value of this variable use {@link #isCursorInsideOfComponent()} method.
 	 * 
 	 *  @see #isCursorInsideOfComponent()
-	 *  @see #bouding
+	 *  @see #component
 	 */
 	private static boolean insideofComponent;
 	
@@ -164,7 +164,7 @@ public class Mouse extends MouseAdapter{
 	 *  and return statement is called. And if bound is <code>false</code> the binding is processed.
 	 * To the specified {@link java.awt.Component Component} is added {@link #INSTANCE instance of this class} as a {@link java.awt.event.MouseListener MouseListener}, 
 	 * {@link java.awt.event.MouseWheelListener MouseWheelListener} and {@link java.awt.event.MouseMotionListener MouseMotionListener}. 
-	 * Reference to the specified {@link java.awt.Component Component} is stored to {@link #bouding}. The {@link #buttons } map is initialized with keys from <code>1</code> to
+	 * Reference to the specified {@link java.awt.Component Component} is stored to {@link #component}. The {@link #buttons } map is initialized with keys from <code>1</code> to
 	 * value obtained by {@link java.awt.MouseInfo#getNumberOfButtons() MouseInfo.getNumberOfButtons()} <code>+ 1</code>. 
 	 * And the {@link #bound} is set to <code>true</code><br><br>
 	 * 
@@ -178,7 +178,7 @@ public class Mouse extends MouseAdapter{
 	 */
 	public static void bind(Component component) {
 		if(bound) {
-			MessageHandler.printMessage("MOUSE", MessageHandler.INFO, "Mouse is already bound to: " + bouding.toString());
+			MessageHandler.printMessage("MOUSE", MessageHandler.INFO, "Mouse is already bound to: " + Mouse.component.toString());
 			return;
 		}
 		
@@ -188,22 +188,22 @@ public class Mouse extends MouseAdapter{
 		
 		int butts = MouseInfo.getNumberOfButtons();
 		for(int i = 0; i < butts; i++) {
-			buttons.put(1 + i, false);
+			buttons.put(i, false);
 		}
 		
-		bouding = component;
+		Mouse.component = component;
 		
 		bound = true;
 	}
 	
 	/**
-	 * Unbinds <code>Mouse</code> from {@link #bouding component} to which the <code>Mouse</code> was bound by {@link #bind(Component)} method 
+	 * Unbinds <code>Mouse</code> from {@link #component component} to which the <code>Mouse</code> was bound by {@link #bind(Component)} method 
 	 * which removes <code>Mouse's</code> ability to listen to various mouse events such as {@link java.awt.event.MouseEvent MouseEvent} and {@link java.awt.event.MouseWheelEvent MouseWheelEvent}.<br>
 	 * <h1>Unbinding</h1>
 	 * If the <code>Mouse</code> was not bound (the {@link #bound} is <code>false</code>), the message is printed through {@link svk.sglubos.engine.utils.debug.MessageHandler MessageHandler} and return statement is called.
-	 * And if the {@link #bound} is <code>true</code> the unbinding is processed. From the {@link #bouding Component } to which <code>Mouse</code> was bound is {@link #INSTANCE} removed as the {@link java.awt.event.MouseListener MouseListener}, 
+	 * And if the {@link #bound} is <code>true</code> the unbinding is processed. From the {@link #component Component } to which <code>Mouse</code> was bound is {@link #INSTANCE} removed as the {@link java.awt.event.MouseListener MouseListener}, 
 	 * as the {@link java.awt.event.MouseWheelListener MouseWheelListener} and as the {@link java.awt.event.MouseMotionListener MouseMotionListener}. 
-	 * The {@link #bouding reference} to the {@link java.awt.Component Component} to which the <code>Mouse</code> was bound is set to <code>null</code>.
+	 * The {@link #component reference} to the {@link java.awt.Component Component} to which the <code>Mouse</code> was bound is set to <code>null</code>.
 	 * And the {@link #bound} is set to <code>false</code>.<br><br> 
 	 * 
 	 * @see #bind()		
@@ -218,11 +218,11 @@ public class Mouse extends MouseAdapter{
 			return;
 		}
 		
-		bouding.removeMouseListener(INSTANCE);
-		bouding.removeMouseMotionListener(INSTANCE);
-		bouding.removeMouseWheelListener(INSTANCE);
+		component.removeMouseListener(INSTANCE);
+		component.removeMouseMotionListener(INSTANCE);
+		component.removeMouseWheelListener(INSTANCE);
 				
-		bouding = null;
+		component = null;
 		bound = false;
 	}
 	
@@ -269,7 +269,7 @@ public class Mouse extends MouseAdapter{
 
 	/**
 	 * Overridden {@link java.awt.event.MouseAdapter#mouseEntered(MouseEvent) MouseAdapter.mouseEntered(MouseEvent) method} from {@link java.awt.event.MouseAdapter MouseAdapter} 
-	 * which is used to handle if the cursor enters the {@link #bouding Component} to which is <code>Mouse</code> bound.<br>
+	 * which is used to handle if the cursor enters the {@link #component Component} to which is <code>Mouse</code> bound.<br>
 	 * <h1>Handling</h1>
 	 * The {@link #insideofComponent} is set to <code>true</code>.
 	 * And the {@link #mouseEventModifiersEx}  is set to {@link java.awt.event.MouseEvent#getModifiersEx() MouseEvent.getModifiersEx()} from this listened event.<br>
@@ -277,7 +277,7 @@ public class Mouse extends MouseAdapter{
 	 * @see #insideofComponent
 	 * @see #isCursorInsideOfComponent()
 	 * @see #mouseEventModifiersEx
-	 * @see #bouding
+	 * @see #component
 	 * @see java.awt.event.MouseEvent
 	 */
 	@Override
@@ -289,7 +289,7 @@ public class Mouse extends MouseAdapter{
 	
 	/**
 	 * Overridden {@link java.awt.event.MouseAdapter#mouseExited(MouseEvent) MouseAdapter.mouseExited(MouseEvent) method} from {@link java.awt.event.MouseAdapter MouseAdapter} 
-	 * which is used to handle if the cursor exits the {@link #bouding Component} to which is <code>Mouse</code> bound.<br>
+	 * which is used to handle if the cursor exits the {@link #component Component} to which is <code>Mouse</code> bound.<br>
 	 * <h1>Handling</h1>
 	 * The {@link #insideofComponent} is set to <code>false</code>.
 	 * And the {@link #mouseEventModifiersEx}  is set to {@link java.awt.event.MouseEvent#getModifiersEx() MouseEvent.getModifiersEx()} from this listened event.<br>
@@ -297,7 +297,7 @@ public class Mouse extends MouseAdapter{
 	 * @see #insideofComponent
 	 * @see #isCursorInsideOfComponent()
 	 * @see #mouseEventModifiersEx
-	 * @see #bouding
+	 * @see #component
 	 * @see java.awt.event.MouseEvent
 	 */
 	@Override
@@ -331,7 +331,7 @@ public class Mouse extends MouseAdapter{
 	
 	/**
 	 * Overridden {@link java.awt.event.MouseAdapter#mouseMoved(MouseEvent) MouseAdapter.mouseMoved(MouseEvent) method} from {@link java.awt.event.MouseAdapter MouseAdapter} 
-	 * which is used to handle if the cursor is moved inside of {@link #bouding Component} to which is <code>Mouse</code> bound.<br>
+	 * which is used to handle if the cursor is moved inside of {@link #component Component} to which is <code>Mouse</code> bound.<br>
 	 * <h1>Handling</h1>
 	 * Sets the value of {@link #x} to value obtained by {@link java.awt.event.MouseEvent#getX() MouseEvent.getX()}, 
 	 * sets the value of {@link #y} to value obtained by {@link java.awt.event.MouseEvent#getY() MouseEvent.getY()}.
@@ -352,41 +352,123 @@ public class Mouse extends MouseAdapter{
 		mouseEventModifiersEx = e.getModifiersEx();
 	}
 	
+	/**
+	 * Sets the {@link #mouseWheelRotation} to <code>0</code> and {@link #mouseWheelRotated} to <code>false</code>.<br>
+	 * The {@link #mouseWheelRotation} can be obtained by {@link #getRotation()} method, 
+	 * which also sets the {@link #mouseWheelRotation} to <code>0</code> and {@link #mouseWheelRotated} to <code>false</code>.
+	 * 
+	 * @see #getRotation()
+	 * @see #mouseWheelRotation
+	 * @see #mouseWheelRotated
+	 */
 	public static void resetMouseWheelRotation() {
 		mouseWheelRotated = false;
 		mouseWheelRotation = 0;
 	}
 	
+	/**
+	 * Determines if specific mouse button is pressed.<br>
+	 * Returns the boolean value stored in {@link java.util.HashMap HashMap} {@link #buttons}, 
+	 * where the integer (parameter of this method) is the key for obtaining the value stored in this {@link java.util.HashMap HashMap}.
+	 * If the mouse button with the specified key is pressed, the returned value is <code>true</code>, otherwise the returned value is <code>false</code>
+	 * The key can be simply the number of mouse button. see {@link java.awt.event.MouseEvent#getButton() MouseEvent.getButton()}
+	 *   
+	 * @param button specified key for obtaining state of mouse button with this key<br><br>
+	 * @return state of button with specified key<br><br>
+	 * 
+	 * @see #buttons
+	 * @see java.awt.event.MouseEvent#getButton()
+	 * @see java.awt.event.MouseEvent
+	 */
 	public static boolean isButtonPressed(int button) {
 		return buttons.get(button);
 	}
 	
+	/**
+	 * Returns the value of {@link #mouseWheelRotation} which contains the rotation of mouse wheel.<br>
+	 * The value of {@link #mouseWheelRotation} contains the rotation accumulated from the last reset of the rotation or last call of this method.
+	 * Also sets the {@link #mouseWheelRotation} to <code>0</code> and sets the {@link #mouseWheelRotated} to <code>false</code>.
+	 * The value of {@link #mouseWheelRotation} and {@link #mouseWheelRotated} can be also reset by {@link #resetMouseWheelRotation()} method.<br>
+	 * 
+	 * @return the rotation of mouse wheel<br><br>
+	 * 
+	 * @see #mouseWheelRotation
+	 * @see #mouseWheelRotated
+	 * @see #resetMouseWheelRotation()
+	 */
 	public static int getRotation() {
 		int rot = mouseWheelRotation;
-		resetMouseWheelRotation();
+		mouseWheelRotation = 0;
+		mouseWheelRotated = false;
 		return rot;
 	}
 	
+	/**
+	 * @return the horizontal coordinate of mouse cursor relative to component to which is <code>Mouse</code> bound<br><br>
+	 * 
+	 * @see #bind(Component)
+	 * @see #unbind()
+	 * @see #component
+	 */
 	public static int getX() {
 		return x;
 	}
 	
+	/**
+	 * @return the vertical coordinate of mouse cursor relative to {@link #component component} to which is <code>Mouse</code> bound<br><br>
+	 * 
+	 * @see #bind(Component)
+	 * @see #unbind()
+	 * @see #component
+	 */
 	public static int getY() {
 		return y;
 	}
 	
+	/**
+	 * @return the value of {@link #bound}, which determines if the <code>Mouse</code> is bound to a {@link #java.awt.Component Component}, 
+	 * if is bound returns <code>true</code> otherwise return <code>false</code><br><br>
+	 * 
+	 * @see #bound
+	 * @see #bind(Component)
+	 * @see #unbind()
+	 */
 	public static boolean isBound() {
 		return bound;
 	}
 	
+	/**
+	 * @return the extended modifiers from the latest {@link java.awt.event.MouseEvent MouseEvent} ({@link java.awt.event.MouseEvent#getModifiersEx() MouseEvent.getModifiersEx()})
+	 * or from the latest {@link java.awt.event.MouseWheelEvent MouseWheelEvent} ({@link java.awt.event.MouseWheelEvent#getModifiersEx() MouseWheelEvent.getModifiersEx()}).<br><br>
+	 * 
+	 * @see #mouseEventModifiersEx
+	 * @see java.awt.event.MouseEvent#getModifiersEx()
+	 * @see java.awt.event.MouseWheelEvent#getModifiersEx()
+	 */
 	public static int getModifiersEx() {
 		return mouseEventModifiersEx;
 	}
 	
+	/**
+	 * @return the value of {@link #insideofComponent}, which determines if the mouse cursor is inside of a {@link #component} to which is mouse bound.<br><br>
+	 * 
+	 * @see #bound
+	 * @see #component
+	 * @see #bind(Component)
+	 * @see #unbind()
+	 */
 	public static boolean isCursorInsideOfComponent() {
 		return insideofComponent;
 	}
 	
+	/**
+	 * @return the value of {@link #mouseWheelRoted}, which determines if the mouse wheel was rotated
+	 * 
+	 * @see #mouseWheelRotated
+	 * @see #mouseWheelRotation
+	 * @see #wasMousewheelRotated()
+	 * @see #getRotation()
+	 */
 	public static boolean wasMousewheelRotated() {
 		return mouseWheelRotated;
 	}
