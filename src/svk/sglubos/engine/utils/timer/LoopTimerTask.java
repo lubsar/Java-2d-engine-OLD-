50bd9b52fc21f99943f9e3c8fcd45a62ef44a9b4
sglubos <5lubo555@azet.sk>
2016-04-09 11:02:07 +0200

    fixed copyright headers

--- src/svk/sglubos/engine/utils/timer/LoopTimerTask.java
+++ src/svk/sglubos/engine/utils/timer/LoopTimerTask.java
@@ -1,5 +1,5 @@
 /*
- *	Copyright 2015 Ä˝ubomĂ­r Hlavko
+ *	Copyright 2015 Ľubomír Hlavko
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
